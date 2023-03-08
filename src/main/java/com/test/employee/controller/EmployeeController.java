b2d9fac13f4e0a088f7abddc77ffbb74dea7bd64
Yasi <113851370+YasiruBasura@users.noreply.github.com>
2023-03-08 13:09:17 +0530

    IP update

--- src/main/java/com/test/employee/controller/EmployeeController.java
+++ src/main/java/com/test/employee/controller/EmployeeController.java
@@ -5,7 +5,7 @@ import com.test.employee.services.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin(origins ="http://localhost:3000" )
+@CrossOrigin(origins ="http://localhost:3001" )
 @RestController
 @RequestMapping("/api/v1/")
 
